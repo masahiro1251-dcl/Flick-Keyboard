29b9ddbe9c044d428e2de4883eef160d5ba082b8
nanoGimlet <gota-nano@akane.waseda.jp>
2021-06-22 17:56:14 +0900

    左の手首にフリックを移動

--- Flick Keyboards/Assets/Scripts/HandPosition.cs
+++ Flick Keyboards/Assets/Scripts/HandPosition.cs
@@ -9,18 +9,21 @@ using Microsoft.MixedReality.Toolkit.UI;
 public class HandPosition : MonoBehaviour
 {
     // Start is called before 
-    public GameObject logger;
-    public GameObject keyboard;
-    private string pos;
     void Start()
     {
-        pos = "";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos = this.transform.position.ToString();
-        logger.GetComponent<TextMesh>().text = pos;
+        if(HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out MixedRealityPose pose))
+        {
+            Debug.Log(pose.Position);
+        }
+        else
+        {
+            Debug.Log("Lost");
+        }
     }
 }
