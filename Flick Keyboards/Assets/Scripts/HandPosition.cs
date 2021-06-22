9d5366b61931dce4d5f379b416d5340ca94d083d
nanoGimlet <gota-nano@akane.waseda.jp>
2021-06-22 17:22:03 +0900

    左の手のひらにフリックを実装

---
+++ Flick Keyboards/Assets/Scripts/HandPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+
+
+public class HandPosition : MonoBehaviour
+{
+    // Start is called before 
+    public GameObject logger;
+    public GameObject keyboard;
+    private string pos;
+    void Start()
+    {
+        pos = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = this.transform.position.ToString();
+        logger.GetComponent<TextMesh>().text = pos;
+    }
+}
