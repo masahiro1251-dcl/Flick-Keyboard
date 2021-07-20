1919a240f019355a7f5a4835a6ddad9431df7f1d
nanoGimlet <gota-nano@akane.waseda.jp>
2021-07-20 13:59:30 +0900

    base image

--- Flick Keyboards/Assets/Scripts/HandPosition.cs
+++ Flick Keyboards/Assets/Scripts/HandPosition.cs
@@ -10,11 +10,8 @@ using Microsoft.MixedReality.Toolkit;
 public class HandPosition : MonoBehaviour
 {
     [SerializeField] GameObject Contents;
-    [SerializeField] float margin;
-    [SerializeField] float dist;
+    [SerializeField] GameObject Specials;
     private Vector3 _estimatedAngularVelocity = Vector3.zero;
-    private Vector3 shaftPosition;
-    private Quaternion shaftRotation;
     private Quaternion pastRotation = Quaternion.identity;
     public Vector3 EstimatedAngularVelocity
     {
@@ -25,70 +22,61 @@ public class HandPosition : MonoBehaviour
     void Start()
     {
         Contents.SetActive(false);
+        Specials.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         var handJointService = CoreServices.GetInputSystemDataProvider<IMixedRealityHandJointService>();
-        if(HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out MixedRealityPose pose))
+        if(HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out MixedRealityPose lpose))
         {
             Contents.SetActive(true);
+            Specials.SetActive(true);
             Transform jointTransform = handJointService.RequestJointTransform(TrackedHandJoint.Wrist, Handedness.Left);
-            shaftPosition = jointTransform.position + jointTransform.forward * (-dist);
-            shaftRotation = jointTransform.rotation;
-            // Contents.transform.position = shaftPosition + jointTransform.forward * (-dist);
-            // Contents.transform.rotation = shaftRotation;
             Quaternion rot = Contents.transform.rotation;
+            Transform palmTransform = handJointService.RequestJointTransform(TrackedHandJoint.Palm, Handedness.Left);
+            Specials.transform.rotation = palmTransform.rotation;
+            Specials.transform.position = palmTransform.position;
+
             if(pastRotation != Quaternion.identity)
             {
+                // 角速度計算、axisが回転軸、angleが回転量
                 Quaternion deltaRotation = Quaternion.Inverse(pastRotation) * jointTransform.rotation;
                 deltaRotation.ToAngleAxis(out float angle, out Vector3 axis);
                 float angularSpeed = (angle * Mathf.Deg2Rad) / Time.deltaTime;
                 _estimatedAngularVelocity = axis * angularSpeed;
 
-                // 角速度がmarginより小さいなら、その方向の角度は固定
+                // 現在のWristに合わせて回転
+                Contents.transform.rotation = jointTransform.rotation;
 
-                Contents.transform.rotation = shaftRotation;
-
-                // 没案（細かく補正をかけようとすると逆に処理が重くなる）
-                // float dx = _estimatedAngularVelocity.x * Time.deltaTime;
-                // float dy = _estimatedAngularVelocity.y * Time.deltaTime;
-                // float dz = _estimatedAngularVelocity.z * Time.deltaTime;
-                // if(dx >= margin)
-                // {
-                //     Vector3 xaxis = new Vector3(axis.x, 0.0f, 0.0f);
-                //     Contents.transform.Rotate(xaxis, -angle);
-                // }
-                // if(dy >= margin)
-                // {
-                //     Vector3 yaxis = new Vector3(0.0f, axis.y, 0.0f);
-                //     Contents.transform.Rotate(yaxis, -angle);
-                // }
-                // if(dz >= margin)
-                // {
-                //     Vector3 zaxis = new Vector3(0.0f, 0.0f, axis.z);
-                //     Contents.transform.Rotate(zaxis, -angle);
-                // }
-                Debug.Log("old " + Contents.transform.rotation.ToString("F4"));
-                // 今、最初の位置で完全に角度が固定されるような実装になっている
-                Contents.transform.Rotate(axis, -angle);
-                Debug.Log("new " + Contents.transform.rotation.ToString("F4"));
-                Contents.transform.position = jointTransform.position;
+                // 入力するタイミングだけ固定されればいい、右手の人差し指をトリガーにする
+                if(HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out MixedRealityPose rpose))
+                {
+                    // Wristの回転分逆回転で戻す
+                    Contents.transform.Rotate(axis, -angle);
+                    Contents.transform.position = jointTransform.position;
+                }
+                else
+                {
+                    // 逆回転せず、位置だけ更新
+                    Contents.transform.position = jointTransform.position;
+                }
             }
             else
             {
+                // Wristに追従する形
                 Contents.transform.rotation = jointTransform.rotation;
                 Contents.transform.position = jointTransform.position;
             }
             pastRotation = Contents.transform.rotation;
-            // Debug.Log("Wrist " + jointTransform.rotation.ToString("F4") + " shaft " + Contents.transform.rotation.ToString("F4"));
+            Debug.Log("Contents " + Contents.transform.position + " Specials " + Specials.transform.position);
         }
         else
         {
             Contents.SetActive(false);
+            Specials.SetActive(false);
             pastRotation = Quaternion.identity;
-            // pastPosition = defaultPosition;
         }
     }
 }
